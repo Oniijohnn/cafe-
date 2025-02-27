eda08bc34bd143b11a6835313405d4afa7b085ba
Onii_johnn <jrebesa@gmail.com>
2025-02-27 12:02:27 +0800

    Patch 129 cmdjs removed index.js

--- deployCommands.js
+++ deployCommands.js
@@ -1,12 +1,26 @@
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const commands = require('./commands');
-require('dotenv').config();
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+import dotenv from 'dotenv';
+import { afkCommands } from './commands/afkCommands.js';
+import { welcomeCommands } from './commands/welcomeCommands.js';
+import { moderationCommands } from './commands/moderationCommands.js';
+import { blacklistCommands } from './commands/blacklistCommands.js';
+import { reportCommand } from './commands/reportCommand.js';
+
+dotenv.config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const TOKEN = process.env.TOKEN;
 
 const rest = new REST({ version: '9' }).setToken(TOKEN);
+
+const commands = [
+  ...afkCommands,
+  ...welcomeCommands,
+  ...moderationCommands,
+  ...blacklistCommands,
+  reportCommand,
+];
 
 (async () => {
   try {
